f7251743b6b7b85cf599805a0ab05d8a2955e46a
giuseppemasi <masi.1962771@studenti.uniroma1.it>
2022-12-09 09:11:50 +0100

    fixed parameter to weight_constraint.py

--- src/models/tabl/weight_constraint.py
+++ src/models/tabl/weight_constraint.py
@@ -1,7 +1,6 @@
 import torch
 
 class WeightConstraint(object):
-
     def __init__(self):
         pass
 
@@ -10,7 +9,7 @@ class WeightConstraint(object):
             # print("Entered")
             w = module.weight.data
             norm = w.norm(2, dim=0, keepdim=True)
-            desired = torch.clamp(norm, min=0.0, max=3.0)
+            desired = torch.clamp(norm, min=0.0, max=5.0)
             w *= (desired / (1e-8 + norm))
             module.weight.data = w
 
