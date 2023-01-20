f377741b7e0dae512c180bced89c676d213d736a
giuseppemasi <masi.1962771@studenti.uniroma1.it>
2023-01-20 13:01:07 +0100

    added num_snapshots to DeepLOBATT

--- src/models/deeplobatt/dlbatt_param_search.py
+++ src/models/deeplobatt/dlbatt_param_search.py
@@ -7,6 +7,7 @@ HP_DEEPATT = {
     cst.LearningHyperParameter.LEARNING_RATE.value: {'max': 0.001, 'min': 0.0001},  # 'max': 0.001, 'min': 0.0001
     cst.LearningHyperParameter.EPS.value: {'values': [1e-07]},  # 'max': 0.001, 'min': 0.0001
     cst.LearningHyperParameter.BATCH_SIZE.value: {'values': [32, 64, 128]},  # [32, 64, 128]
+    cst.LearningHyperParameter.NUM_SNAPSHOTS.value: {'values': [50]},  # [32, 64, 128]
 }
 
 
@@ -16,4 +17,5 @@ HP_DEEPATT_FI_FIXED = {
     cst.LearningHyperParameter.LEARNING_RATE.value: 0.001,
     cst.LearningHyperParameter.EPS.value: 1e-07,
     cst.LearningHyperParameter.BATCH_SIZE.value: 32,
+    cst.LearningHyperParameter.NUM_SNAPSHOTS.value: 50,
 }
