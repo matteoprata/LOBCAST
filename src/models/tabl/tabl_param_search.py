7138fae8f9055d81a000bbec5efe2fa0765b18eb
Giuseppe <masi.1962771@studenti.uniroma1.it>
2023-04-24 17:04:15 +0200

    fixed parameters for ctabl

--- src/models/tabl/tabl_param_search.py
+++ src/models/tabl/tabl_param_search.py
@@ -6,8 +6,8 @@ HP_TABL = {
     cst.LearningHyperParameter.OPTIMIZER.value: {'values': [cst.Optimizers.ADAM.value]},
     cst.LearningHyperParameter.NUM_SNAPSHOTS.value: {'values': [10]},
 
-    cst.LearningHyperParameter.BATCH_SIZE.value: {'values': [32, 64, 128]},
-    cst.LearningHyperParameter.LEARNING_RATE.value: {'values': [1e-2, 1e-3, 1e-4, 1e-5]}
+    cst.LearningHyperParameter.BATCH_SIZE.value: {'values': [64]},
+    cst.LearningHyperParameter.LEARNING_RATE.value: {'values': [0.001]}
 }
 
 HP_TABL_FI_FIXED = {
